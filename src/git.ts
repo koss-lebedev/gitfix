603cb31862adaafe9862277ce0bf25040fc8f29f
Constantine Lebedev <koss.lebedev@gmail.com>
2019-04-07 11:06:58 +0200

    Add remove command

--- src/git.ts
+++ src/git.ts
@@ -3,8 +3,12 @@ import { pipe } from 'ramda'
 
 const execute = (command: string) => execa.shellSync(command)
 
-const filterCommand = (command: string) => `
-  git filter-branch --tree-filter "${command}" -f -- --all
+const filterTreeCommand = (command: string) => `
+  git filter-branch --force --tree-filter "${command}" -- --all
+`
+
+const filterIndexCommand = (command: string) =>  `
+  git filter-branch --force --index-filter '${command}' --prune-empty --tag-name-filter cat -- --all
 `
 
 const appendContent = (filePath: string, content: string) => `
@@ -13,9 +17,20 @@ const appendContent = (filePath: string, content: string) => `
   EOF
 `
 
+const removeFile = (filePath: string) => `
+  git rm --cached --ignore-unmatch ${filePath}
+`
+
 export const appendCommand = (filePath: string, content: string) =>
   pipe(
     appendContent,
-    filterCommand,
+    filterTreeCommand,
     execute
   )(filePath, content)
+
+export const removeCommand = (filePath: string) =>
+  pipe(
+    removeFile,
+    filterIndexCommand,
+    execute,
+  )(filePath)
